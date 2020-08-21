f758b707798308c1e1c70b0056f8be6fc1d12b64
Serik <70020897+ayaz-js@users.noreply.github.com>
2020-08-21 18:43:14 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// --> Begin Gallery MagnificPopup
+
+$(document).ready(function() {
+  $('.ps-gallery__zoom').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300,
+      opener: function(element) {
+      return element.find('img');
+      }
+    }    
+  });
+});
+
+// --> End Gallery MagnificPopup
+
+// --> Begin Slick Slider
+
+$(document).ready(function(){
+    $('.ps-reviews__box').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      dots: true,
+    });
+});
+
+// --> End Slick Slider
